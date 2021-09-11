0e08f89066edf3e0fef6dd3d94b91547e6457b7f
Tommy Wilczek <tommywilczek@utexas.edu>
2021-09-11 11:03:40 -0500

    connects to contract and gets colors

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from '../logo.png';
 import './App.css';
-import detectEthereumProvider from '@metamask/detect-provider'
+import Color from '../abis/Color.json';
+import { ethers, Contract } from 'ethers';
 
 class App extends Component {
 
@@ -11,29 +12,59 @@ class App extends Component {
   }
 
   async loadWeb3() {
-    const provider = await detectEthereumProvider()
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    this.setState({ provider });
 
-    if (provider) {
-      console.log('Ethereum successfully detected!')
-      // Legacy providers may only have ethereum.sendAsync
-      const chainId = await provider.request({
-        method: 'eth_chainId'
-      })
-    } else {
-      console.error('Please install MetaMask!')
-    }
   }
 
   async loadBlockchainData() {
-    const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-    const account = accounts[0];
+    console.log('this.state.provider :>> ', this.state.provider);
+    await this.state.provider.send("eth_requestAccounts", []);
+    const signer = this.state.provider.getSigner();
+    const account = await signer.getAddress();
     this.setState({ account });
+
+    const balance = await this.state.provider.getBalance(account);
+    const formattedBalance = ethers.utils.formatEther(balance);
+    this.setState({ balance: formattedBalance });
+
+    const networkData = Color.networks[window.ethereum.networkVersion];
+    if (networkData) {
+      const colorContract = new Contract(
+        networkData.address,
+        Color.abi,
+        signer
+      )
+      this.setState({ colorContract });
+      console.log('this.state.colorContract :>> ', this.state.colorContract);
+      
+      const totalSupply = await colorContract.totalSupply().then(bigNum => bigNum.toNumber());
+      this.setState({ totalSupply });
+      console.log('this.state.totalSupply :>> ', this.state.totalSupply);
+      
+      for (let i = 0; i < totalSupply; i++) {
+        let c = await colorContract.colors(i);
+        this.setState({
+          colors: [... this.state.colors, c]
+        })
+      }
+
+      console.log('this.state.colors :>> ', this.state.colors);
+
+    } else {
+      window.alert('We have detected you switched networks. Please switch to the Ethereum Mainnet.')
+    }
   }
 
   constructor(props) {
     super(props);
     this.state = {
-      account: ''
+      provider: undefined,
+      account: undefined,
+      balance: undefined,
+      colorContract: undefined,
+      totalSupply: 0,
+      colors: []
     };
   }
 
@@ -51,35 +82,34 @@ class App extends Component {
           </a>
           <ul className="navbar-nav px-3">
             <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-              <small className="text-white"><span id="account">{this.state.account}</span></small>
+              <img src={logo} className="App-logo" alt="logo" />
+            </li>
+          </ul>
+          <ul className="navbar-nav px-3">
+            <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
+              <small className="text-white"><span id="account">Account: {this.state.account}</span></small>
+              <hr />
+              <small className="text-white"><span id="balance">Balance: {this.state.balance}</span></small>
             </li>
           </ul>
         </nav>
+        {/* Hacky formatting... */}
+        <br/>
+        <br/>
+        <br/>
+        <br/>
+        <br/>
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                {/* FORM GOES HERE */}
               </div>
             </main>
+          </div>
+          <hr />
+          <div className="row text-center">
+            <p>Tokens go here...</p>
           </div>
         </div>
       </div>
